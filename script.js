e6bf74607c17db051b23fb81989f1c20407eb512
lokified <lsheldon645@gmail.com>
2022-04-06 16:36:18 +0300

    add contact functionality

--- script.js
+++ script.js
@@ -20,3 +20,64 @@ closeBtn.addEventListener('click', () => {
     menu.style.transition = "all 800ms ease-in-out"
     closeBtn.style.display = "none";
 })
+
+
+//send email
+
+let loader = document.querySelector(".loader");
+    let firstName = document.querySelector(".firstName");
+    let lastName = document.querySelector(".lastName");
+    let email = document.querySelector(".email");
+    let msg = document.querySelector(".message");
+    let btn = document.querySelector(".submit");
+
+
+function validate() {
+    
+
+    btn.addEventListener("click", (e) => {
+      e.preventDefault();
+      if (firstName.value === "" || lastName.value === "" || email.value === "" || msg.value === "") {
+        emptyerror();
+      } else {
+        loader.style.display = "flex";
+        sendmail(email.value, msg.value);
+        success();
+        loader.style.display = "none";
+      }
+    });
+  }
+  validate();
+
+  function sendmail(email, msg) {
+    emailjs.send("service_p3gm9x7", "template_imb2zsc", {
+      to_name: "Sheldon",
+      from_name: email,
+      message: msg,
+    });
+  }
+
+  function emptyerror() {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Fields cannot be empty!",
+    });
+  }
+
+  function error() {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Something went wrong!",
+    });
+  }
+
+  function success() {
+    Swal.fire({
+      icon: "success",
+      title: "Success...",
+      text: "Successfully sent message",
+    });
+}
+
